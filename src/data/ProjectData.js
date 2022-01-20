ff809f9260e876de872e73291d6c4841e424bcb7
Ray Nimalan <raymondnimalan@gmail.com>
2022-01-20 13:35:51 -0500

    Mask on Project added

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -2,6 +2,16 @@ import InfuaxWarsMockup from '../images/InfauxMockup.jpeg';
 import PoshPupsMockup from '../images/PoshPupsMockup.jpeg';
 export const projectData = [
   {
+    title: 'MaskOn',
+    about1:
+      'Responsive web application using label detection through machine learning to verify if a person is wearing a mask to prevent the spread of COVID-19.',
+    about2:
+      'Tech Stack: JavaScript, React, Redux, Express, Node.js, Google Vision API',
+    image: InfuaxWarsMockup,
+    websiteUrl: 'http://mask-on-app.herokuapp.com/',
+    githubUrl: 'https://github.com/RaymondNimalan/MaskOn',
+  },
+  {
     title: 'Infaux Wars',
     about1:
       'Progressive web application using Google machine learning to determine the reliability of online news articles.',
